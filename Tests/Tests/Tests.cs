93ebc40e166fd39a226a6c0db43915e93e21dcd8
Sergio Flores <relfos@gmail.com>
2020-05-18 17:37:51 +0100

    Tests now run VM

--- Tests/Tests/Tests.cs
+++ Tests/Tests/Tests.cs
@@ -1,6 +1,10 @@
 using NUnit.Framework;
 using Phantasma.CodeGen.Assembler;
 using Phantasma.Tomb.Compiler;
+using Phantasma.VM;
+using Phantasma.VM.Utils;
+using System;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -9,6 +13,154 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
+        }
+
+        public class TestVM : VirtualMachine
+        {
+            private Dictionary<string, Func<ExecutionFrame, ExecutionState>> _interops = new Dictionary<string, Func<ExecutionFrame, ExecutionState>>();
+            private Func<string, ExecutionContext> _contextLoader;
+            private Dictionary<string, ScriptContext> contexts;
+            private Dictionary<byte[], byte[]> storage = new Dictionary<byte[], byte[]>();
+
+            public TestVM(byte[] script) : base(script)
+            {
+                RegisterContextLoader(ContextLoader);
+                RegisterInterop("Data.Set", Data_Set);
+                RegisterInterop("Data.Get", Data_Get);
+                RegisterInterop("Data.Delete", Data_Delete);
+                contexts = new Dictionary<string, ScriptContext>();
+            }
+
+            private ExecutionContext ContextLoader(string contextName)
+            {
+                if (contexts.ContainsKey(contextName))
+                    return contexts[contextName];
+
+                return null;
+            }
+
+            public byte[] BuildScript(string[] lines)
+            {
+                IEnumerable<Semanteme> semantemes = null;
+                try
+                {
+                    semantemes = Semanteme.ProcessLines(lines);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Error parsing the script");
+                }
+
+                var sb = new ScriptBuilder();
+                byte[] script = null;
+
+                try
+                {
+                    script = sb.ToScript();
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Error assembling the script");
+                }
+
+                return script;
+            }
+
+            public void RegisterInterop(string method, Func<ExecutionFrame, ExecutionState> callback)
+            {
+                _interops[method] = callback;
+            }
+
+            public void RegisterContextLoader(Func<string, ExecutionContext> callback)
+            {
+                _contextLoader = callback;
+            }
+
+            public override ExecutionState ExecuteInterop(string method)
+            {
+                if (_interops.ContainsKey(method))
+                {
+                    return _interops[method](this.CurrentFrame);
+                }
+
+                throw new NotImplementedException();
+            }
+
+            public override ExecutionContext LoadContext(string contextName)
+            {
+                if (_contextLoader != null)
+                {
+                    return _contextLoader(contextName);
+                }
+
+                throw new NotImplementedException();
+            }
+
+            public override void DumpData(List<string> lines)
+            {
+                // do nothing
+            }
+
+            public void Expect(bool condition, string description)
+            {
+                if (condition)
+                {
+                    return;
+                }
+
+                throw new VMException(this, description);
+            }
+
+
+            private ExecutionState Data_Get(ExecutionFrame frame)
+            {
+                var key = this.Stack.Pop();
+                var key_bytes = key.AsByteArray();
+
+                this.Expect(key_bytes.Length > 0, "invalid key");
+
+                var value_bytes = storage[key_bytes];
+                var val = new VMObject();
+                val.SetValue(value_bytes, VMType.Bytes);
+                this.Stack.Push(val);
+
+                return ExecutionState.Running;
+            }
+
+            private ExecutionState Data_Set(ExecutionFrame frame)
+            {
+                var key = this.Stack.Pop();
+                var key_bytes = key.AsByteArray();
+
+                var val = this.Stack.Pop();
+                var val_bytes = val.AsByteArray();
+
+                this.Expect(key_bytes.Length > 0, "invalid key");
+
+                var firstChar = (char)key_bytes[0];
+                this.Expect(firstChar != '.', "permission denied"); // NOTE link correct PEPE here
+
+                this.storage[key_bytes] = val_bytes;
+
+                return ExecutionState.Running;
+            }
+
+            private ExecutionState Data_Delete(ExecutionFrame frame)
+            {
+                var key = this.Stack.Pop();
+                var key_bytes = key.AsByteArray();
+
+                this.Expect(key_bytes.Length > 0, "invalid key");
+
+                var firstChar = (char)key_bytes[0];
+                this.Expect(firstChar != '.', "permission denied"); // NOTE link correct PEPE here
+
+                this.storage.Remove(key_bytes);
+
+                return ExecutionState.Running;
+            }
+
+
         }
 
         [Test]
@@ -34,7 +186,14 @@ namespace Tests
 
             var script = AssemblerUtils.BuildScript(lines);
 
-            Assert.Pass();
+            var vm = new TestVM(script);
+            vm.Stack.Push(VMObject.FromObject("test()"));
+            var result = vm.Execute();
+
+
+            var obj = vm.Stack.Pop();
+
+            Assert.IsTrue(result == ExecutionState.Halt);
         }
     }
 }
