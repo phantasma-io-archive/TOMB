406230d50f71d49505dfa8d3d6ea9d88a7557c5d
Sergio Flores <relfos@gmail.com>
2020-11-12 22:28:08 +0000

    Added unit test for bools

--- Tests/Tests/Tests.cs
+++ Tests/Tests/Tests.cs
@@ -484,5 +484,38 @@ namespace Tests
             Assert.IsTrue(newVal == expectedVal);
         }
 
+        [Test]
+        public void TestBool()
+        {
+            string[] sourceCode = new string[] {
+                "token TEST {",
+                "global _contractPaused:bool;",
+                "property name: string = \"Ghost\";	",
+                "   constructor(owner:address)	{" ,
+                "       _contractPaused:= false;" ,
+                "}}"
+            };
+
+            var parser = new Compiler();
+            var contract = parser.Process(sourceCode).First();
+
+            var storage = new Dictionary<byte[], byte[]>(new ByteArrayComparer());
+
+            TestVM vm;
+
+            var constructor = contract.abi.FindMethod(SmartContract.ConstructorName);
+            Assert.IsNotNull(constructor);
+
+            var keys = PhantasmaKeys.Generate();
+
+            vm = new TestVM(contract, storage, constructor);
+            vm.ThrowOnFault = true;
+            vm.Stack.Push(VMObject.FromObject(keys.Address));
+            var result = vm.Execute();
+            Assert.IsTrue(result == ExecutionState.Halt);
+
+            Assert.IsTrue(storage.Count == 1);
+        }
+
     }
 }
