6065061bd555a2eb9680729a54b176225cb6ef80
merl111 <mathias@bloctec.dev>
2020-11-30 21:42:51 +0100

    fix, fixed tests

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -771,34 +771,50 @@ namespace Tests
             var sourceCode =
                 @"struct someStruct
                 {
-created:timestamp;
-creator:address;
-royalties:number;
-name:string;
-description:string;
-imageURL:string;
-infoURL:string;
+                    created:timestamp;
+                    creator:address;
+                    royalties:number;
+                    name:string;
+                    description:string;
+                    imageURL:string;
+                    infoURL:string;
                 }
-            token " + symbol + @" {
-                import Runtime;
-                import Time;
-                import NFT;
-                global _address:address;
-                global _owner:address;
+                token " + symbol + @" {
+                    import Runtime;
+                    import Time;
+                    import NFT;
+                    global _address:address;
+                    global _owner:address;
 
-                property name:string = """+ name + @""";
+                    property name:string = """+ name + @""";
 
-                nft myNFT<someStruct, number> {
-                    property name:string {
-                        return _ROM.name;
+                    nft myNFT<someStruct, number> {
+                        property name:string {
+                            return _ROM.name;
+                        }
+
+                        property description:string {
+                            return _ROM.description;
+                        }
+
+                        property imageURL:string {
+                            return _ROM.imageURL;
+                        }
+
+                        property infoURL:string {
+                            return _ROM.infoURL;
+                        }
                     }
 
-                    property description:string {
-                        return _ROM.description;
+                    constructor(owner:address)	{
+                        _address := @P2KEYzWsbrMbPNtW1tBzzDKeYxYi4hjzpx4EfiyRyaoLkMM;
+                        _owner:= owner;
+                        NFT.createSeries(owner, $THIS_SYMBOL, 0, 999, TokenSeries.Unique, myNFT);
                     }
 
-                    property imageURL:string {
-                        return _ROM.imageURL;
+                    public mint(dest:address):number {
+                        local rom:someStruct := Struct.someStruct(Time.now(), _address, 1, ""hello"", ""desc"", ""imgURL"", ""info"");
+                        return NFT.mint(_address, dest, $THIS_SYMBOL, rom, 0, 0);
                     }
 
                     public readName(nftID:number): string {
@@ -810,24 +826,7 @@ infoURL:string;
                         local nftInfo:NFT := NFT.read($THIS_SYMBOL, nftID);
                         return nftInfo.owner;
                     }
-                }
-
-                constructor(owner:address)	{
-_address := @P2KEYzWsbrMbPNtW1tBzzDKeYxYi4hjzpx4EfiyRyaoLkMM;
-_owner:= owner;
-       NFT.createSeries(owner, $THIS_SYMBOL, 0, 999, TokenSeries.Unique, myNFT);
-                }
-
-                public mint(dest:address):number {
-                    local rom:someStruct := Struct.someStruct(Time.now(), _address, 1, ""hello"", ""desc"", ""imgURL"", ""info"");
-                    return NFT.mint(_address, dest, $THIS_SYMBOL, rom, 0, 0);
-                }
-
-                public read(nftID:number): string {
-                    local nftInfo:someStruct := NFT.readROM<someStruct>($THIS_SYMBOL, nftID);
-                    return nftInfo.name;
-                }
-            }";
+                }";
 
             var parser = new Compiler();
             var contract = parser.Process(sourceCode).First();
@@ -1144,53 +1143,55 @@ _owner:= owner;
             simulator.EndBlock();
         }
 
-        [Test]
-        public void TestGHOST()
-        {
-            var keys = PhantasmaKeys.Generate();
-            var keys2 = PhantasmaKeys.Generate();
+        // add simplified version of that test
+        //[Test]
+        //public void TestGHOST()
+        //{
+        //    var keys = PhantasmaKeys.Generate();
+        //    var keys2 = PhantasmaKeys.Generate();
 
-            var nexus = new Nexus("simnet", null, null);
-            nexus.SetOracleReader(new OracleSimulator(nexus));
-            var simulator = new NexusSimulator(nexus, keys, 1234);
-            var mempool = new Mempool(simulator.Nexus, 2, 1, System.Text.Encoding.UTF8.GetBytes("TEST"), 0, new DummyLogger());
-            mempool?.SetKeys(keys);
+        //    var nexus = new Nexus("simnet", null, null);
+        //    nexus.SetOracleReader(new OracleSimulator(nexus));
+        //    var simulator = new NexusSimulator(nexus, keys, 1234);
+        //    var mempool = new Mempool(simulator.Nexus, 2, 1, System.Text.Encoding.UTF8.GetBytes("TEST"), 0, new DummyLogger());
+        //    mempool?.SetKeys(keys);
 
-            var api = new NexusAPI(simulator.Nexus);
-            api.Mempool = mempool;
-            mempool.Start();
-            var sourceCode = System.IO.File.ReadAllLines("/home/merl/source/phantasma/GhostMarketContractPhantasma/GHOST.tomb");
-            var parser = new Compiler();
-            var contract = parser.Process(sourceCode).First();
-            Console.WriteLine("contract asm: " + contract.abi);
+        //    var api = new NexusAPI(simulator.Nexus);
+        //    api.Mempool = mempool;
+        //    mempool.Start();
+        //    var sourceCode = System.IO.File.ReadAllLines("/home/merl/source/phantasma/GhostMarketContractPhantasma/GHOST.tomb");
+        //    var parser = new Compiler();
+        //    var contract = parser.Process(sourceCode).First();
+        //    //Console.WriteLine("contract asm: " + contract.asm);
+        //    //System.IO.File.WriteAllText(@"GHOST_series.asm", contract.SubModules.First().asm);
 
-            simulator.BeginBlock();
-            simulator.GenerateCustomTransaction(keys, ProofOfWork.Minimal,
-                    () => ScriptUtils.BeginScript().AllowGas(keys.Address, Address.Null, 1, 9999)
-                    .CallInterop("Nexus.CreateToken", keys.Address, "GHOST", "GHOST", new BigInteger(10000), new BigInteger(0),
-                        TokenFlags.Transferable|TokenFlags.Burnable|TokenFlags.Finite, contract.script, contract.abi.ToByteArray())
-                    .SpendGas(keys.Address)
-                    .EndScript());
-            simulator.EndBlock();
+        //    simulator.BeginBlock();
+        //    simulator.GenerateCustomTransaction(keys, ProofOfWork.Minimal,
+        //            () => ScriptUtils.BeginScript().AllowGas(keys.Address, Address.Null, 1, 9999)
+        //            .CallInterop("Nexus.CreateToken", keys.Address, "GHOST", "GHOST", new BigInteger(10000), new BigInteger(0),
+        //                TokenFlags.Transferable|TokenFlags.Burnable|TokenFlags.Finite, contract.script, contract.abi.ToByteArray())
+        //            .SpendGas(keys.Address)
+        //            .EndScript());
+        //    simulator.EndBlock();
 
-            var token = (TokenResult)api.GetToken("GHOST");
-            Console.WriteLine("id: " + token.ToString());
-            Console.WriteLine("address: " + token.address);
+        //    var token = (TokenResult)api.GetToken("GHOST");
+        //    Console.WriteLine("id: " + token.ToString());
+        //    Console.WriteLine("address: " + token.address);
 
-            simulator.BeginBlock();
-            simulator.GenerateCustomTransaction(keys, ProofOfWork.Minimal,
-                    () => ScriptUtils.BeginScript().AllowGas(keys.Address, Address.FromText(token.address), 1, 9999)
-                    .CallContract("GHOST", "mintToken", 0, 1, 1,
-                        keys.Address, 0, "GHOST", 1, "testnft", "desc1234567890", 1,
-                        "0", "0", "", "", "", "", "", "", "", 0, "", new Timestamp(1), "", 0)
-                    .SpendGas(keys.Address)
-                    .EndScript());
-            simulator.EndBlock();
+        //    simulator.BeginBlock();
+        //    simulator.GenerateCustomTransaction(keys, ProofOfWork.Minimal,
+        //            () => ScriptUtils.BeginScript().AllowGas(keys.Address, Address.FromText(token.address), 1, 9999)
+        //            .CallContract("GHOST", "mintToken", 0, 1, 1,
+        //                keys.Address, 0, "GHOST", 1, "testnft", "desc1234567890", 1,
+        //                "0", "0", "", "", "", "", "", "", "", 0, "", new Timestamp(1), "", 0)
+        //            .SpendGas(keys.Address)
+        //            .EndScript());
+        //    simulator.EndBlock();
 
-            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-            var nft = (TokenDataResult)api.GetNFT("GHOST", "80807712912753409015029052615541912663228133032695758696669246580757047529373", true);
-            Console.WriteLine("nft series: " + nft.series);
-        }
+        //    Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        //    var nft = (TokenDataResult)api.GetNFT("GHOST", "80807712912753409015029052615541912663228133032695758696669246580757047529373", true);
+        //    Console.WriteLine("nft series: " + nft.series);
+        //}
 
         [Test]
         public void TestCROWN()
@@ -1213,7 +1214,6 @@ _owner:= owner;
             Console.WriteLine("address: " + token.address);
 
             simulator.TimeSkipDays(200);
-            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
             var nft = (TokenDataResult)api.GetNFT("CROWN", "64648043722874601761586352284082823113174122931185981250820896676646424691598", true);
             Console.WriteLine("nft series: " + nft.properties.ToString());
             foreach (var a in nft.properties)
@@ -1221,8 +1221,6 @@ _owner:= owner;
                 Console.WriteLine($"res {a.Key}:{a.Value}");
 
             }
-
-            throw new Exception();
         }
     }
 }
