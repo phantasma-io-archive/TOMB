a6bf48339b5a097b2fbd8feb4dfd5305767983ac
João Novo <34069419+TeknoPT@users.noreply.github.com>
2023-09-21 14:56:27 +0100

    Adeded new tests

--- Library/tests/Contracts/StructTests.cs
+++ Library/tests/Contracts/StructTests.cs
@@ -1,6 +1,77 @@
+using System.Numerics;
+using Nethereum.Util;
+using Phantasma.Core.Domain.Execution.Enums;
+using Phantasma.Core.Domain.VM;
+using Phantasma.Tomb.Compilers;
+
 namespace TOMBLib.Tests.Contracts;
 
 public class StructTests
 {
+    private struct MyStruct
+    {
+        public string name;
+        public BigInteger age;
+    }
     
+    [Test]
+    public void TestStructChanging()
+    {
+        var sourceCode =
+            @"
+struct my_local_struct {
+    name:string;
+    age:number;
+}
+
+contract test{
+    import Struct;          
+    public testMyStruct (name:string, age:number) : my_local_struct {
+        local myStruct : my_local_struct = Struct.my_local_struct(name, age);
+        if ( myStruct.age == 10 ) {
+            myStruct.age = 20;
+        }
+        return myStruct;
+    }
+}";
+
+        var parser = new TombLangCompiler();
+        var contract = parser.Process(sourceCode).First();
+
+        var storage = new Dictionary<byte[], byte[]>(new ByteArrayComparer());
+
+        TestVM vm;
+        var method = contract.abi.FindMethod("testMyStruct");
+        // Age 10
+        var myStruct = new MyStruct();
+        myStruct.name = "John";
+        myStruct.age = 10;
+        vm = new TestVM(contract, storage, method);
+        vm.Stack.Push(VMObject.FromObject(myStruct.name));
+        vm.Stack.Push(VMObject.FromObject(myStruct.age));
+        var result = vm.Execute();
+        Assert.IsTrue(result == ExecutionState.Halt);
+
+        Assert.IsTrue(vm.Stack.Count == 1);
+
+        var obj = vm.Stack.Pop();
+        var returnObject = obj.AsStruct<MyStruct>();
+        Assert.AreEqual(myStruct.name,returnObject.name );
+        Assert.AreEqual(myStruct.age, (BigInteger)20);
+        
+        myStruct.name = "BartSimpson";
+        myStruct.age = 50;
+        vm = new TestVM(contract, storage, method);
+        vm.Stack.Push(VMObject.FromObject(myStruct.name));
+        vm.Stack.Push(VMObject.FromObject(myStruct.age));
+        result = vm.Execute();
+        Assert.IsTrue(result == ExecutionState.Halt);
+
+        Assert.IsTrue(vm.Stack.Count == 1);
+
+        obj = vm.Stack.Pop();
+        returnObject = obj.AsStruct<MyStruct>();
+        Assert.AreEqual(myStruct.name,returnObject.name );
+        Assert.AreEqual(myStruct.age, returnObject.age);
+    }
 }
--- Library/tests/Contracts/SwitchTest.cs
+++ Library/tests/Contracts/SwitchTest.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using NUnit.Framework;
+using Phantasma.Core.Cryptography;
 using Phantasma.Core.Domain;
 using Phantasma.Core.Domain.Execution.Enums;
 using Phantasma.Core.Domain.VM;
@@ -12,7 +14,7 @@ namespace TOMBLib.Tests.Contracts;
 public class SwitchTest
 {
     [Test]
-    public void Switch()
+    public void SwitchNumber()
     {
         var sourceCode =
             @"
@@ -53,6 +55,115 @@ contract test {
             }
 
             Assert.IsTrue(result == expected);
+        }
+    }
+
+    [Test]
+    public void TestSwitchString()
+    {
+        var sourceCode =
+            @"
+contract test {
+    public check(x:string): number {
+        switch (x) {
+            case ""zero"": return 0;
+            case ""one"": return 1;
+            case ""two"": return 2;
+            default: return 99;
+        }                  
+     }}";
+
+        var parser = new TombLangCompiler();
+        var contract = parser.Process(sourceCode).First();
+
+        var storage = new Dictionary<byte[], byte[]>(new ByteArrayComparer());
+
+        var check = contract.abi.FindMethod("check");
+        Assert.IsNotNull(check);
+
+        // test different cases
+        for (int i = 0; i <= 4; i++)
+        {
+            var vm = new TestVM(contract, storage, check);
+            var str = i.ToString();
+            switch (i)
+            {
+                case 0: str = "zero"; break;
+                case 1: str = "one"; break;
+                case 2: str = "two"; break;
+                default: str = "other"; break;
+            }
+            
+            vm.Stack.Push(VMObject.FromObject(str));
+            var state = vm.Execute();
+            Assert.IsTrue(state == ExecutionState.Halt);
+            var result = vm.Stack.Pop().AsNumber();
+
+            BigInteger expected = i;
+            if ( i > 2 )
+            {
+                expected = 99;
+            }
+            
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+    [Test]
+    public void TestSwitchDecimal()
+    {
+        var sourceCode =
+            @"
+contract test {
+    public check(x:decimal<2>): decimal<2> {
+        switch (x) {
+            case 1.0: return x;
+            case 1.1: return x;
+            default: return x;
+        }                  
+     }}";
+
+        var parser = new TombLangCompiler();
+        var contract = parser.Process(sourceCode).First();
+
+        var storage = new Dictionary<byte[], byte[]>(new ByteArrayComparer());
+
+        var check = contract.abi.FindMethod("check");
+        Assert.IsNotNull(check);
+
+        // test different cases
+        for (int i = 0; i <= 4; i++)
+        {
+            var vm = new TestVM(contract, storage, check);
+            decimal _value = 1.1m;
+            switch (i)
+            {
+                case 0: _value = 1.00m; break;
+                case 1: _value = 1.10m; break;
+                case 2:  _value = 1.20m; break;
+                default: _value = 1.50m; break;
+            }
+            
+            vm.Stack.Push(VMObject.FromObject( _value));
+            var state = vm.Execute();
+            Assert.IsTrue(state == ExecutionState.Halt);
+            var obj = vm.Stack.Pop();
+            Assert.Fail(obj.AsNumber());
+            
+            // TODO: Finish this one.
+            /* 
+            var result = obj.AsNumber();
+
+            decimal expected = _value;
+            switch (i)
+            {
+                case 0: expected += 1 ; break;
+                case 1: expected += 2; break;
+                case 2:  expected += 3; break;
+                default: expected += 5m; break;
+            }*/
+            
+           // Assert.AreEqual(expected, result);
         }
     }
 }
